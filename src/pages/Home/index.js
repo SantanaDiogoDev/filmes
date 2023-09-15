c7ad73cb195e6e4cf738ebb6efc92dfc83cd1088
Diogo Santana <diogo.santana.dev@gmail.com>
2023-09-15 17:30:14 -0300

    filmes aula get filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api';
+
+//movie/now_playing?api_key=0357fd07df267e396ccdb3a8686fe6f0&language=pt-br
+
 function Home(){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect( () =>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: "0357fd07df267e396ccdb3a8686fe6f0",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            console.log(response.data.results);
+        }
+
+        loadFilmes();
+    });
+
     return(
         <div>
             <h1>Home</h1>
---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+//Base
+// https://api.themoviedb.org/3/
+//API now playing
+//https://api.themoviedb.org/3/movie/now_playing?api_key=0357fd07df267e396ccdb3a8686fe6f0&language=pt-br
+
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+export default api;
