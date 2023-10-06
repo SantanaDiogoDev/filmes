4cefaf6a60c68fe8dbb77cd0061c269eae782afe
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2023-10-06 17:18:51 -0300

    List de favoritos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header>
             <Link className='logo' to="/">Prime Flix</Link>
-            <Link className='favoritos' to="/">Favoritos</Link>
+            <Link className='favoritos' to="/favoritos">Favoritos</Link>
         </header>
     )
 }
