aa7c618bd1f138e294182718de18f806aa7beb61
SantanaDiogoDev <diogo.santana.dev@gmail.com>
2023-10-16 14:39:55 -0300

    alterando icon pelo React Helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import RoutesApp from "./routes";
 import { ToastContainer } from 'react-toastify';
+import { Helmet } from 'react-helmet';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Projeto Filmes - Santana</title>
+        <link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
+      </Helmet>
       <ToastContainer
         position="top-right"
         autoClose={3000}
